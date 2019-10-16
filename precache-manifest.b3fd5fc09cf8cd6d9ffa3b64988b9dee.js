f75462245781f13f199e589bbcf11dce197731e3
Amin Ajao <aminajao96@gmail.com>
2019-10-16 11:52:41 +0100

    Updates

---
+++ precache-manifest.b3fd5fc09cf8cd6d9ffa3b64988b9dee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a2fd9613b9d37e0581ec2b7bb6b3827",
+    "url": "/reacTodoAnimated/index.html"
+  },
+  {
+    "revision": "7cf0434a28619a64f7cc",
+    "url": "/reacTodoAnimated/static/css/main.355df82a.chunk.css"
+  },
+  {
+    "revision": "78448feaafabb56fe557",
+    "url": "/reacTodoAnimated/static/js/2.d3353c0e.chunk.js"
+  },
+  {
+    "revision": "7cf0434a28619a64f7cc",
+    "url": "/reacTodoAnimated/static/js/main.28964cdb.chunk.js"
+  },
+  {
+    "revision": "7db52a444fc083647a20",
+    "url": "/reacTodoAnimated/static/js/runtime-main.dfc36c05.js"
+  }
+]);
